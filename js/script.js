66b0525b820d2778e2b214b8abe3e0eaa06c3a3c
Najima Musa <najmusaa@gmail.com>
2024-01-05 17:28:47 +0000

    adding nav to every page, new pages

--- js/script.js
+++ js/script.js
@@ -41,6 +41,19 @@ $(document).ready(function () {
 	fetchBestsellersBooks();
 });
 
+var mySwiper = new Swiper(".swiper-container", {
+	direction: "vertical",
+	loop: true,
+	pagination: ".swiper-pagination",
+	grabCursor: true,
+	speed: 1000,
+	paginationClickable: true,
+	parallax: true,
+	autoplay: false,
+	effect: "slide",
+	mousewheelControl: 1
+  });
+
 // $("#book-search-form").submit(function (e) {
 // 	e.preventDefault();
 
