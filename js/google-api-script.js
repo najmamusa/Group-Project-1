73db85dcca398cc33b8f3d01c38a9e6b4f109943
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-01-05 16:10:43 +0000

    feat: Add updated style and code and code clean up

--- js/google-api-script.js
+++ js/google-api-script.js
@@ -14,11 +14,12 @@ $("#book-search-form").submit(function (e) {
 	fetch(queryURL)
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
 			if (data.items) {
 				$("#search-term").val("");
 				$("#book-results").empty();
-				data.items.forEach((item) => {
+
+				let currentRow = $("<div>").addClass("row");
+				data.items.forEach((item, index) => {
 					if (item.volumeInfo) {
 						let title = item.volumeInfo.title || "No title available";
 						let author = item.volumeInfo.authors
@@ -35,35 +36,44 @@ $("#book-search-form").submit(function (e) {
 							: "No page count available";
 						let publisher = item.volumeInfo.publisher
 							? item.volumeInfo.publisher
-							: "No page count available";
+							: "No publisher available";
 						let category = item.volumeInfo.categories
 							? item.volumeInfo.categories
 							: "No category available";
-						// let description =
-						// 	item.searchInfo && item.searchInfo.textSnippet
-						// 		? item.searchInfo.textSnippet
-						// 		: "No description available";
-						let description =
-							item.volumeInfo.description && item.volumeInfo.description
-								? item.volumeInfo.description
-								: "No description available";
+						let description = item.volumeInfo.description
+							? item.volumeInfo.description
+							: "No description available";
 						let thumbnail = item.volumeInfo.imageLinks
 							? item.volumeInfo.imageLinks.smallThumbnail
 							: "path_to_default_image.jpg";
 
-						let column = $("<div>").addClass("col-md-4");
-						let card = $("<div>").addClass("card").css("border", "none");
+						let column = $("<div>").addClass("col-md-4 p-5");
+						let card = $("<div>")
+							.addClass("card d-flex flex-column border border")
+							.css({
+								"border-radius": "15%",
+								"margin-top": "40px",
+								"margin-bottom": "40px",
+								"min-height": "450px",
+
+								display: "flex",
+								"flex-direction": "column",
+								"justify-content": "space-between",
+							});
 						let imgElement = $("<img>")
 							.addClass("card-img-top")
 							.attr("src", thumbnail)
 							.attr("alt", title)
 							.css({ "max-height": "350px", "object-fit": "contain" });
 						let cardBody = $("<div>").addClass("card-body");
-						let titleElement = $("<h5>").addClass("card-title").text(title);
+						let titleElement = $("<h5>")
+							.addClass("card-title text-center")
+							.text(title);
 						let modalTriggerButton = $("<button>")
-							.addClass("btn btn-primary")
+							.addClass("btn btn-primary d-block mx-auto ")
 							.attr("data-bs-toggle", "modal")
 							.attr("data-bs-target", "#bookModal")
+							.css("margin-top", "20px")
 							.text("More Details")
 							.click(function () {
 								$("#bookModalLabel").html(title);
@@ -95,7 +105,12 @@ $("#book-search-form").submit(function (e) {
 
 						card.append(cardBody);
 						column.append(card);
-						$("#book-results").append(column);
+
+						if (index % 3 === 0 && index !== 0) {
+							currentRow = $("<div>").addClass("row");
+							$("#book-results").append(currentRow);
+						}
+						currentRow.append(column);
 					}
 				});
 			} else {
